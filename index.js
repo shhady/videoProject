b1c06d3538796eb549229117381cbdb25b367279
Abu.khater90 <abu.khater90@gmail.com>
2023-07-16 18:20:57 +0300

    first commit

--- index.js
+++ index.js
@@ -2,10 +2,4 @@
 
 
 
-cho "# videoProject" >> README.md
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/Abukhater90/videoProject.git
-git push -u origin main
+
