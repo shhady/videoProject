a5354e23468af130ce502aefd3411cc113ef7256
amalshweiki <amalshweiki88@gmail.com>
2023-07-27 16:55:15 +0300

    extra-loop

---
+++ js/objects/index.js
@@ -0,0 +1,76 @@
+//1: Creating the Movie Database
+movieDatabase ={
+movies:[],
+
+//ex 1.3
+addMovie : function(MovieTitle){
+  this.movies.push({title:MovieTitle, ratings:[],averageRatings:0})
+                               },
+//ex 1.4
+removeMovie : function (MovieTitle) {
+ //this.movies.filter(movie => movie.title !==MovieTitle)
+  this.movies=this.movies.filter(movie => movie.title !== MovieTitle)
+                                    },  
+//ex 2.1
+addRating : function (MovieTitle,rating) {
+  const movie = this.movies.find((movie)=> movie.title === MovieTitle)
+  console.log(movie)
+  if(movie){
+    movie.ratings.push(rating)
+   this.calculateAverageRating(MovieTitle)
+  }
+//  for(let movie of this.movies) {
+//     if(movie.title==MovieTitle){movie.ratings.push(rating)}
+//                                }
+                                         },
+//ex 2.2
+removeRating : function (MovieTitle,ratingMovie) {
+    for(let movie of this.movies) {
+                  if(movie.title==MovieTitle){
+                     movie.ratings =movie.ratings.filter( rating => rating !== ratingMovie)
+                     this.calculateAverageRating(MovieTitle)
+                    }
+                                  }
+                                                 },
+
+ //ex 3.1                                    
+ calculateAverageRating : function (MovieTitle){
+    const movie = this.movies.find((movie)=> movie.title === MovieTitle)
+    if(movie){
+        
+       const sum = movie.ratings.reduce((acc, ra)=> acc + ra ,0)
+       movie.averageRatings = movie.ratings.length > 0 ? sum/movie.ratings.length : 0;
+      }
+ }                  }                                         
+                                        
+                                
+                                        
+console.log("ex 1.3")                                        
+movieDatabase.addMovie("abc")
+console.log(movieDatabase.movies)
+
+console.log("ex 1.4")  
+movieDatabase.removeMovie("abc")
+console.log(movieDatabase.movies)
+
+//add element to array to use in the next ex
+console.log("add element to array to use in the next ex")  
+movieDatabase.addMovie("abc")
+console.log(movieDatabase.movies)
+
+console.log("ex 2.1")  
+
+movieDatabase.addRating("abc",6)
+console.log(movieDatabase.movies)
+
+console.log("ex 2.2")  
+movieDatabase.removeRating("abc",6)
+console.log(movieDatabase.movies)
+
+////add element rating
+movieDatabase.addRating("abc",2)
+movieDatabase.addRating("abc",4)
+
+console.log("ex 3.1")  
+movieDatabase.calculateAverageRating("abc")
+console.log(movieDatabase.movies)
