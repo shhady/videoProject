39e46bcb5b66fddd5c7b3d378df4841dc787cce7
Abu.khater90 <abu.khater90@gmail.com>
2023-07-28 16:31:48 +0300

    Objects

--- JavaScript/Objects/object.js
+++ JavaScript/Objects/object.js
@@ -11,30 +11,36 @@ const movieDatabase =
 
     } ,
 
-    removeMovie : function ( title  ){ this.movies = this.movies.filter (this.movies === title ) } ,
+    removeMovie : function ( title  ){ 
+        this.movies = this.movies.filter ((movie) => movie.title !== title ) 
+    } ,
 
-    addRating : function (titleMovie , rating){ this.title.include(this.title===titleMovie )}
+
+    addRating : function (title , rating){
+        // console.log({moviesArr:this.movies})
+        const movie = this.movies.find((movie)=> movie.title === title )
+        if(movie){
+            movie.ratings.push(rating)
+            console.log(movie)
+        }
+    } 
+    // this.title.include(this.title === titleMovie )}
     
 }
 
-movieDatabase.addMovie('fast & f',[], 0)
-movieDatabase.addMovie('gladiator',[2,1,4], 3)
+movieDatabase.addMovie('fast',[], 0)
+// movieDatabase.addMovie('gladiator',[2,1,4], 3)
 // {title :"" , ratings :null , averageRating:null }
 
 // console.log(movieDatabase.movies)
 
 
 
-addRating: function (title, rating) {
-    const movie = this.movies.find(movie => movie.title === title);
-    if (movie) {
-      movie.ratings.push(rating);
-    }
-
 
 // exercise 2 :
 
- movieDatabase.addRating();
+movieDatabase.addRating("fast", 6)
+// console.log(movieDatabase)
 
 
 
@@ -44,3 +50,18 @@ addRating: function (title, rating) {
 
 
 
+
+
+
+
+ /*  addRating : function (MovieTitle,rating) {
+  //  if (this.movies.title == MovieTitle ){this.movies.ratings.push(rating)}
+  const movie = this.movies.find((movie)=> movie.title === MovieTitle)
+  console.log(movie)
+  if(movie){
+    movie.ratings.push(rating)
+    const sum = movie.ratings.reduce((acc, ra)=> acc + ra ,0)
+    movie.averageRatings = movie.ratings.length > 0 ? sum/movie.ratings.length : 0;
+  } 
+
+  */
