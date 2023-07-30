4857a5d9e9fe808b50c6d3afc38ccde248b30a91
amalshweiki <amalshweiki88@gmail.com>
2023-07-31 02:28:22 +0300

    object update

--- js/objects/index.js
+++ js/objects/index.js
@@ -17,7 +17,7 @@ addRating : function (MovieTitle,rating) {
   console.log(movie)
   if(movie){
     movie.ratings.push(rating)
-   this.calculateAverageRating(MovieTitle)
+   this.calculateAverageRating(MovieTitle) //ex 3.2
   }
 //  for(let movie of this.movies) {
 //     if(movie.title==MovieTitle){movie.ratings.push(rating)}
@@ -28,7 +28,7 @@ removeRating : function (MovieTitle,ratingMovie) {
     for(let movie of this.movies) {
                   if(movie.title==MovieTitle){
                      movie.ratings =movie.ratings.filter( rating => rating !== ratingMovie)
-                     this.calculateAverageRating(MovieTitle)
+                     this.calculateAverageRating(MovieTitle) //ex 3.2
                     }
                                   }
                                                  },
@@ -40,9 +40,57 @@ removeRating : function (MovieTitle,ratingMovie) {
        const sum = movie.ratings.reduce((acc, ra)=> acc + ra ,0)
        movie.averageRatings = movie.ratings.length > 0 ? sum/movie.ratings.length : 0;
              }
-                                               }     
-                                                         }                                         
-                                        
+                                               },     
+
+  //ex 4 
+ 
+
+  searchingMovies : function (stringToSearch){
+    const array=[]
+    for (const movie of this.movies) {
+      const array1 =movie.title.split(''); 
+           if(array1.includes(stringToSearch)){
+               array.push(movie.title)
+                  return array
+                             }
+      
+                 else{const str="no searching results";
+                 return str;}
+                                     }
+                                              } ,
+ //ex 5.1  Sorting Movies
+ SortingMoviesByRatings : function () {
+
+  this.movies.sort((movie1, movie2) => {
+   
+    if (movie2.averageRatings !== movie1.averageRatings) {
+      return movie2.averageRatings - movie1.averageRatings;
+    } 
+    if (movie1.title < movie2.title) return -1;
+    if (movie1.title > movie2.title) return 1;
+    return 0;
+  });
+ // return this.movies.sort((movie1, movie2) => movie2.averageRatings - movie1.averageRatings);
+ },
+
+  SortingMoviesByTitle : function () {
+
+   this.movies.sort((movie1, movie2) => {
+      const title1 = movie1.title.toUpperCase(); 
+      const title2 = movie2.title.toUpperCase(); 
+      if (title1< title2) {
+        return -1;
+      }
+      if (title1 > title2) {
+        return 1;
+      }
+      return 0;
+    });
+  
+ },                                                
+                                            }                                         
+   
+
                                 
                                         
 console.log("ex 1.3")                                        
@@ -73,4 +121,26 @@ movieDatabase.addRating("abc",4)
 
 console.log("ex 3.1")  
 movieDatabase.calculateAverageRating("abc")
+console.log(movieDatabase.movies)
+
+
+console.log("ex 4")
+console.log(movieDatabase.searchingMovies("a"))
+console.log(movieDatabase.searchingMovies("f"))
+
+movieDatabase.addMovie("xyz")
+console.log(movieDatabase.movies)
+movieDatabase.addRating("xyz",10)
+movieDatabase.addRating("xyz",8)
+movieDatabase.addMovie("efg")
+movieDatabase.addRating("efg",6)
+movieDatabase.addRating("efg",4)
+console.log(movieDatabase.movies)
+//ex 5.1
+console.log("ex 5.1")
+movieDatabase.SortingMoviesByRatings ()
+console.log(movieDatabase.movies)
+
+console.log("ex 5.2")
+movieDatabase.SortingMoviesByTitle ()
 console.log(movieDatabase.movies)
