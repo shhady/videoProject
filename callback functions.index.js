4c0918a90a75a70816040db3a244e7fcbe487e36
Ori Baram <obrm770@gmail.com>
2023-07-31 12:49:51 +0300

    Code Review

--- callback functions.index.js
+++ callback functions.index.js
@@ -17,13 +17,26 @@ function convertToUpperCase(movie, callback) {      /////   1
   let movie2= { title: "Movie 1", rating:8};
   console.log(ratingisNumber(movie2,rating=> (typeof (movie2.rating)!=="number"?"This movie doesn't have rating" : 
   `${movie2.title} has a numerical rating of ${movie2.rating}`)));
-  
+
+/*
+    CR - should be:
+        function ratingisNumber(movie, callback) {       ///////   2
+          if (callback(movie)) {
+            console.log(`${movie.title} has a numerical rating of ${movie.rating}`)
+          } else {
+            "This movie doesn't have a numerical rating"
+          }
+        }
+
+let movie2 = { title: "Movie 1", rating: 8 };
+console.log(ratingisNumber(movie2, rating => (typeof (rating) !== "number")))
+*/
 
 
   function addExclamation(title,callback){      /////    3
     return callback(title);
   }
-console.log(addExclamation(movie1,title=>`${movie1.title}!`));
+console.log(addExclamation(movie1,title=>`${movie1.title}!`)); // CR- shold be console.log(addExclamation(movie1, title => `${title}!`));    
 
 function reverseWords(description,callback){
  return callback(description);
